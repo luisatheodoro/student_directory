2ab26619710ffa8a5469ba0ac82ce4e28f9cb5e5
luisatheodoro <theodoro.luisa@gmail.com>
2018-10-25 11:35:43 +0000

    creating a method that checks with the user  if input was given and is correct

--- directory.rb
+++ directory.rb
@@ -27,6 +27,26 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+def check_if_input_was_given_and_is_correct(input, type_of_data)
+  until !input.empty?
+    puts "You left the field blank, write the #{type_of_data} again"
+    input = gets.chomp
+  end
+  while true
+    puts "Is #{input} correct?(y/n)"
+    y_n = gets.chomp
+    if y_n == "n"
+      puts "write the #{type_of_data} again"
+      input = gets.chomp
+    elsif y_n == "y"
+      break
+    else
+      puts "Type only y or n"
+    end
+  end
+  return input
+end
+
 print_header
 print_students_list
 print_footer
