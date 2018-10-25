36e5fff327944caf640c951b904520cb4e3e0ceb
luisatheodoro <theodoro.luisa@gmail.com>
2018-10-25 11:28:28 +0000

    adding a method to collect user input and store it inside the students hash

--- directory.rb
+++ directory.rb
@@ -23,10 +23,24 @@ def print(students)
   end
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
